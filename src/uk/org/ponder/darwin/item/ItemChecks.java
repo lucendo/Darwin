c4417a6abcce1045c65ffd7405b545a38ebdca75
Antranig Basman <amb26@ponder.org.uk>
2006-10-18 01:51:51 +0000

    Item Checks, stopwords, version for the BBC!!

--- src/uk/org/ponder/darwin/item/ItemChecks.java
+++ src/uk/org/ponder/darwin/item/ItemChecks.java
@@ -7,38 +7,38 @@ import java.util.Iterator;
 
 import uk.org.ponder.util.Logger;
 
-
 /**
  * @author Antranig Basman (amb26@ponder.org.uk)
- *  
+ * 
  */
 public class ItemChecks {
 
-  public static CheckStatistics checkCollection(ItemCollection collection, CheckStatistics togo) {
+  public static CheckStatistics checkCollection(ItemCollection collection,
+      CheckStatistics togo) {
     for (Iterator itemit = collection.getItems().iterator(); itemit.hasNext();) {
       ItemDetails details = (ItemDetails) itemit.next();
 
       for (int i = 1; i < details.pages.size(); ++i) {
-        String location = "Page with sequence " + i + " of item with ID " + details.ID;
+        String location = "Page with sequence " + i + " of item with ID "
+            + details.ID;
         PageInfo page = (PageInfo) details.pages.get(i);
         try {
-        if (page == null) {
-          togo.errors.add(location + " is missing all data");
-          continue;
-        }
-        String pagetext = page.text == null? "(unknown)" : page.text;
-        location = "page " + pagetext + " with sequence " + i + " of item with ID "
-        + details.ID;
-        if (page == null) {
-          togo.errors.add("No information for " + location);
-        }
-        else {
+          if (page == null) {
+            togo.errors.add(location + " is missing all data");
+            continue;
+          }
+          String pagetext = page.text == null ? "(unknown)"
+              : page.text;
+          location = "page " + pagetext + " with sequence " + i
+              + " of item with ID " + details.ID;
+
           ++togo.pages;
           if (page.contentfile == null && page.imagefile == null) {
-            togo.errors.add("Couldn't find either image or content data for " + location);
+            togo.errors.add("Couldn't find either image or content data for "
+                + location);
           }
           if (page.contentfile == null) {
-            //togo.errors.add("Could not find content file for " + location);
+            // togo.errors.add("Could not find content file for " + location);
           }
           else {
             details.hastext = true;
@@ -49,13 +49,14 @@ public class ItemChecks {
             }
           }
           if (page.imagefile == null) {
-            //togo.errors.add("Could not find image for " + location);
+            if (details.hasimage) {
+              togo.errors.add("Image missing in sequence for " + location);
+            }
           }
           else {
             details.hasimage = true;
             ++togo.images;
           }
-        }
         }
         catch (Exception e) {
           togo.errors.add("Unexpected error " + e + " scanning " + location);
--- src/uk/org/ponder/darwin/parse/Attributes.java
+++ src/uk/org/ponder/darwin/parse/Attributes.java
@@ -3,6 +3,8 @@
  */
 package uk.org.ponder.darwin.parse;
 
+import java.util.Map;
+
 /**
  * @author Antranig Basman (amb26@ponder.org.uk)
  * 
@@ -16,4 +18,11 @@ public class Attributes {
   public static String DOCUMENT_CLASS = "document";
   public static String PAGE_CLASS = "page";
   public static String SECTION_CLASS = "section";
+  
+  public static void cleanseDarwin(Map attrmap) {
+    attrmap.remove(CLASS_ATTR);
+    attrmap.remove(PAGESEQ_ATTR);
+    attrmap.remove(SEQPAGERANGE_ATTR);
+    attrmap.remove(ID_ATTR);
+  }
 }
